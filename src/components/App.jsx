4b629e1df48440f4e44a6ddda1e1b0374ce45dcc
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-07-05 16:49:08 +0300

    task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,12 +24,11 @@ export default class App extends Component {
 		return Math.round(percentage);
 	};
 
-	onLeaveFeedback = (e) => {
-		const name = e.target.name;
-		this.setState((prevState) => ({
-			[name]: prevState[name] + 1
-		}));
-	};
+	onLeaveFeedback = (option) => {
+        this.setState((prevState) => ({
+            [option]: prevState[option] + 1
+        }));
+    };
 	render() {
 		const { good, neutral, bad } = this.state;
 		const total = this.countTotalFeedback();
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -6,7 +6,11 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 	return (
 		<>
 			{options.map((option) => (
-				<button key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
+				<button 
+				key={shortid.generate()} 
+				type="button" 
+				name={option} 
+				onClick={() => onLeaveFeedback(option)}>
 					{option}
 				</button>
 			))}
